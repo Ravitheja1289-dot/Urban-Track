a18ced34f549e8477705b3cec0c0acd61a82be85
Ravitheja Reddy <ravithejareddy1289@gmail.com>
2025-03-15 00:50:31 +0530

    commit

--- my-react-app/backend/main.py
+++ my-react-app/backend/main.py
@@ -1,17 +1,34 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
+import cv2
+from ultralytics import YOLO
 
-app = FastAPI()
+model = YOLO("yolov8n.pt")  
 
-# Enable CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Change this to your frontend URL if needed
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-@app.post("/upload/")
-async def upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+cap = cv2.VideoCapture("Night Traffic on Indian city road.mp4")  
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame)
+
+    for r in results:
+        for box in r.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])  
+            label = model.names[int(box.cls[0])]  
+            conf = round(float(box.conf[0]), 2)  
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            text = f"{label} {conf}"
+            cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255,0 ), 2)
+
+    
+    cv2.imshow("Traffic Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+    print(frame)
+
+cap.release()
+cv2.destroyAllWindows()
